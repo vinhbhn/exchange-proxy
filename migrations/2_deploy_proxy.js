9d7d28f93c01261b9d2b482064e23a86b7fc05dd
vinhbhn <bvinhhn@gmail.com>
2021-05-13 18:00:51 +0700

    redeploy on mumbai

--- migrations/2_deploy_proxy.js
+++ migrations/2_deploy_proxy.js
@@ -2,12 +2,10 @@ const ExchangeProxy = artifacts.require("ExchangeProxy");
 const WETH9 = artifacts.require("WETH9");
 
 module.exports = async function(deployer, network, accounts) {
-    if (network == 'development' || network == 'coverage') {
+    if (network == 'development' || network == 'coverage' || network == 'mumbai' || network == 'polygon') {
         await deployer.deploy(WETH9);
         await deployer.deploy(ExchangeProxy, WETH9.address);
     } else if (network == 'kovan-fork' || network == 'kovan') {
         deployer.deploy(ExchangeProxy, '0xd0A1E359811322d97991E03f863a0C30C2cF029C');
-    } else if (network == 'mumbai') {
-       await deployer.deploy(ExchangeProxy, '0x106e4f8A8f19EC7Da1F5CAFe306FeE9fb0159503');
-    }
+    } 
 }
--- truffle-config.js
+++ truffle-config.js
@@ -1,36 +1,7 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * truffleframework.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
-
+require('dotenv').config();
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
-    /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
     networks: {
         development: {
             host: 'localhost', // Localhost (default: none)
@@ -48,7 +19,7 @@ module.exports = {
         },
         mumbai: {
             // eslint-disable-next-line max-len
-            provider: () => new HDWalletProvider({ privateKeys: ['4c0acdd4dd280274e048a8a4140e103c9d555fba5fc30a79f7ffa3e61b293421'], providerOrUrl: 'https://rpc-mumbai.maticvigil.com' }),
+            provider: () => new HDWalletProvider(process.env.MNEMONIC, 'https://rpc-mumbai.maticvigil.com'),
             network_id: 80001,
             gasPrice: 3000000000, // 3 gwei
             confirmations: 2,
@@ -57,7 +28,7 @@ module.exports = {
         },
         polygon: {
             // eslint-disable-next-line max-len
-            provider: () => new HDWalletProvider({ privateKeys: ['4c0acdd4dd280274e048a8a4140e103c9d555fba5fc30a79f7ffa3e61b293421'], providerOrUrl: 'https://rpc-mainnet.maticvigil.com' }),
+            provider: () => new HDWalletProvider(process.env.MNEMONIC, 'https://rpc-mainnet.maticvigil.com'),
             network_id: 137,
             gasPrice: 3000000000,
             confirmations: 2,
